2953ebf14b9d8ce6b108d3b78cb698d572f53364
Andrey Pronin <moonfly.msk@gmail.com>
2014-10-23 22:46:51 -0400

    Add main tests

--- spec/store_complex_spec.rb
+++ spec/store_complex_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
-class Something < ActiveRecord::Base
+class ARSomething < ActiveRecord::Base
+end
+
+class Something
+  extend StoreComplex::Accessor
   def test_hstore
-    @test_hstore ||= {}
+    $test_hstore ||= {}
+  end
+  def test_hstore=(value)
+    $test_hstore = value
   end
   store_complex :test_hstore, :test_attr
 end
+
 
 RSpec.describe 'StoreComplex::obj_to_store' do
   it 'converts nil to nil' do
@@ -51,7 +59,77 @@ RSpec.describe StoreComplex do
   it 'has a version (smoke test)' do
     expect(StoreComplex::VERSION).to be_a String
   end
+end
+
+RSpec.describe StoreComplex::Accessor do
+  let(:obj) { Something.new }
+  let(:obj1) { Something.new }
   it 'injects its methods into ActiveRecord' do
-    expect(Something.methods).to include *StoreComplex::Accessor.instance_methods
+    expect(ARSomething.methods).to include *StoreComplex::Accessor.instance_methods
+  end
+  it 'allows to store arrays in the store' do
+    value = [1,'a',true]
+    obj.test_attr = value
+    expect( obj.test_attr ).to eq value
+  end
+  it 'allows to store hashes in the store' do
+    value = {'a'=>'b','c'=>12}
+    obj.test_attr = value
+    expect( obj.test_attr ).to eq value
+  end
+  it 'allows to store sets in the store as arrays' do
+    value = Set[1,'a',true]
+    obj.test_attr = value
+    expect( obj.test_attr ).to eq value.to_a
+  end
+  it 'allows to store strings wrapped in an array' do
+    value = 'string'
+    obj.test_attr = value
+    expect( obj.test_attr ).to eq [value]
+  end
+  it 'allows to store integers wrapped in an array' do
+    value = 198
+    obj.test_attr = value
+    expect( obj.test_attr ).to eq [value]
+  end
+  it 'converts symbols to strings when storing' do
+    value = [:a,:b]
+    obj.test_attr = value
+    expect( obj.test_attr ).to eq value.map(&:to_s)
+
+    value = {a:1}
+    obj.test_attr = value
+    expect( obj.test_attr ).to eq value.map { |k,v| [k.to_s,v] }.to_h
+  end
+  it 'detects changes to the stored hashes' do
+    obj.test_attr = {'a'=>2,'b'=>4}
+    obj.test_attr.delete('a')
+    expect( obj1.test_attr ).to eq( {'b'=>4} )
+  end
+  it 'detects changes to the stored arrays' do
+    obj.test_attr = [1,10,8,55]
+    obj.test_attr.sort!
+    expect( obj1.test_attr ).to eq [1,8,10,55]
+  end
+  it 'detects changes to the stored sub-arrays' do
+    obj.test_attr = [[1,10,8,55],[2,19,88,7]]
+    obj.test_attr[1].sort!
+    expect( obj1.test_attr ).to eq [[1,10,8,55],[2,7,19,88]]
+  end
+  it 'detects changing an element in a sub-element' do
+    obj.test_attr = { 'a' => [1,2], 'b' => [3,4], 'c' => [5,6] }
+    obj.test_attr['b'][1] = 10
+    expect( obj1.test_attr ).to eq( { 'a' => [1,2], 'b' => [3,10], 'c' => [5,6] } )
+  end
+  it 'detects changes to the added element of a stored array' do
+    obj.test_attr = [[1,10,8,55],[2,19,88,7]]
+    obj.test_attr << [3,0,7]
+    obj.test_attr[2].sort!
+    expect( obj1.test_attr ).to eq [[1,10,8,55],[2,19,88,7],[0,3,7]]
+  end
+  it 'detects changes to the stored strings' do
+    obj.test_attr = ['some string']
+    obj.test_attr[0].upcase!
+    expect( obj1.test_attr ).to eq ['SOME STRING']
   end
 end
